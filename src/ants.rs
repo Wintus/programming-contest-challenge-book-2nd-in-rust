2e0ba71667e64576bb3ed4f95363b0200e0ff705
Yuya <yuya.maemichi@gmail.com>
2017-06-18 04:01:31 +0900

    Add test case for Ants problem

--- src/ants.rs
+++ src/ants.rs
@@ -1,12 +1,16 @@
+use std::cmp;
+
 struct Problem {
     premise: Premise
 }
 
 struct Premise {
     rod: Rod,
-    ants: [Ant],
+    ants: Vec<Ant>,
 }
 
+#[derive(Debug)]
+#[derive(Eq)]
 struct Solution {
     min: u32,
     max: u32,
@@ -23,5 +27,36 @@ struct Ant {
 }
 
 impl Problem {
-    fn solve(&self) -> Solution {}
+    fn solve(&self) -> Solution {
+        Solution {
+            min: 0,
+            max: 0,
+        }
+    }
+}
+
+impl cmp::PartialEq for Solution {
+    fn eq(&self, other: &Solution) -> bool {
+        self.min == other.min && self.max == other.max
+    }
+}
+
+#[test]
+fn test_case_0() {
+    let premise = Premise {
+        rod: Rod { length: 10 },
+        ants: vec![
+            Ant { x: 2 },
+            Ant { x: 6 },
+            Ant { x: 7 },
+        ]
+    };
+    let solution = Solution {
+        // [-1, 1, 1]
+        min: 4,
+        // [1, 1, 1]
+        max: 8,
+    };
+    let problem = Problem { premise: premise };
+    assert_eq!(solution, problem.solve());
 }
