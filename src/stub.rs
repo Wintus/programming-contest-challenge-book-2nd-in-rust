7229502f266cbb2f580d3e707fe623f394395ba9
Yuya <yuya.maemichi@gmail.com>
2017-06-18 05:38:59 +0900

    Add stub of problem

---
+++ src/stub.rs
@@ -0,0 +1,29 @@
+use std::cmp;
+
+struct Problem {
+    premise: Premise
+}
+
+struct Premise {}
+
+#[derive(Debug)]
+#[derive(Eq)]
+struct Solution {}
+
+impl cmp::PartialEq for Solution {
+    fn eq(&self, other: &Solution) -> bool { true }
+}
+
+impl Problem {
+    fn solve(&self) -> Solution {
+        Solution {}
+    }
+}
+
+#[test]
+fn test_case_0() {
+    let premise = Premise {};
+    let solution = Solution {};
+    let problem = Problem { premise: premise };
+    assert_eq!(solution, problem.solve());
+}
