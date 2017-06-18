011ed5386e0f802a467ed32258372176861c0567
Yuya M <Wintus@users.noreply.github.com>
2017-06-19 02:13:25 +0900

    Solve/triangle (#6)
    * Add triangle problem with copy from stub
    
    * fix stub
    
    * Implement solution of triangle problem
    
    * Add another test case

---
+++ src/triangle.rs
@@ -0,0 +1,53 @@
+use std::cmp;
+
+struct Problem {
+    premise: Premise
+}
+
+struct Premise {
+    sides: Vec<u32>
+}
+
+impl Problem {
+    fn solve(&self) -> u32 {
+        let mut max_perimeter = 0;
+        let ref sides = self.premise.sides;
+
+        // TODO: use permutation
+        let last_index = sides.len() - 1;
+        for i in 0..last_index {
+            for j in (i + 1)..last_index {
+                for k in (j + 1)..last_index {
+                    let sides: [u32; 3] = [
+                        *sides.get(i).unwrap(),
+                        *sides.get(j).unwrap(),
+                        *sides.get(k).unwrap(),
+                    ];
+
+                    let perimeter = sides.iter().sum::<u32>();
+                    let max_side = *sides.iter().max().unwrap();
+                    let rest = perimeter - max_side;
+
+                    if max_side < rest {
+                        max_perimeter = cmp::max(max_perimeter, perimeter);
+                    };
+                }
+            }
+        }
+        max_perimeter
+    }
+}
+
+#[test]
+fn test_case_0() {
+    let premise = Premise { sides: vec![2, 3, 4, 5, 10] };
+    let problem = Problem { premise: premise };
+    assert_eq!(12, problem.solve());
+}
+
+#[test]
+fn test_case_1() {
+    let premise = Premise { sides: vec![4, 5, 10, 20] };
+    let problem = Problem { premise: premise };
+    assert_eq!(0, problem.solve()); // = never
+}
