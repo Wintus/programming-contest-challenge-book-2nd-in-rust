aa255cbb5322f6ed79098cc03a8d8ec4bfc46bc4
Yuya <yuya.maemichi@gmail.com>
2017-06-21 03:18:16 +0900

    Refactor triangle problem by itertools

--- src/triangle.rs
+++ src/triangle.rs
@@ -1,4 +1,6 @@
-use std::cmp;
+extern crate itertools;
+
+use itertools::Itertools;
 
 struct Problem {
     premise: Premise
@@ -8,32 +10,29 @@ struct Premise {
     sides: Vec<u32>
 }
 
+const N_TRIANGLE_SIDES: u8 = 3;
+
 impl Problem {
     fn solve(&self) -> u32 {
-        let mut max_perimeter = 0;
+        let none = 0;
         let ref sides = self.premise.sides;
 
-        // TODO: use combinations
-        let len = sides.len();
-        for i in 0..len {
-            for j in (i + 1)..len {
-                for k in (j + 1)..len {
-                    let sides: [u32; 3] = [
-                        *sides.get(i).unwrap(),
-                        *sides.get(j).unwrap(),
-                        *sides.get(k).unwrap(),
-                    ];
+        let combs: itertools::Combinations<_> =
+            sides.into_iter()
+                 .combinations(N_TRIANGLE_SIDES as usize);
 
-                    let perimeter = sides.iter().sum::<u32>();
-                    let max_side = *sides.iter().max().unwrap();
-                    let rest = perimeter - max_side;
+        let max_perimeter = combs
+            .filter_map(|ns| {
+                let sides = ns.into_iter();
+                let perimeter = sides.clone().sum::<u32>();
+                let max_side = *sides.clone().max().unwrap();
+                let rest = perimeter - max_side;
 
-                    if max_side < rest {
-                        max_perimeter = cmp::max(max_perimeter, perimeter);
-                    };
-                }
-            }
-        }
+                if max_side < rest { Some(perimeter) } else { None }
+            })
+            .max()
+            .unwrap_or(none);
+
         max_perimeter
     }
 }
