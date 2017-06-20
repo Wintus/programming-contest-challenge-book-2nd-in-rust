9d32a7c25450da2130869b6a0e8a5de002e99ea1
Yuya <yuya.maemichi@gmail.com>
2017-06-21 03:22:07 +0900

    Refactor triangle problem rational
    失敗しうる計算を表す

--- src/triangle.rs
+++ src/triangle.rs
@@ -13,25 +13,20 @@ struct Premise {
 const N_TRIANGLE_SIDES: u8 = 3;
 
 impl Problem {
-    fn solve(&self) -> u32 {
-        let none = 0;
+    fn solve(&self) -> Option<u32> {
         let ref sides = self.premise.sides;
-
         let combs: itertools::Combinations<_> =
             sides.into_iter()
                  .combinations(N_TRIANGLE_SIDES as usize);
 
-        let max_perimeter = combs
-            .filter_map(|ns| {
-                let sides = ns.into_iter();
-                let perimeter = sides.clone().sum::<u32>();
-                let max_side = *sides.clone().max().unwrap();
-                let rest = perimeter - max_side;
+        let max_perimeter = combs.filter_map(|ns| {
+            let sides = ns.into_iter();
+            let perimeter = sides.clone().sum::<u32>();
+            let max_side = *sides.clone().max().unwrap();
+            let rest = perimeter - max_side;
 
-                if max_side < rest { Some(perimeter) } else { None }
-            })
-            .max()
-            .unwrap_or(none);
+            if max_side < rest { Some(perimeter) } else { None }
+        }).max();
 
         max_perimeter
     }
@@ -41,12 +36,12 @@ impl Problem {
 fn test_case_0() {
     let premise = Premise { sides: vec![2, 3, 4, 5, 10] };
     let problem = Problem { premise: premise };
-    assert_eq!(12, problem.solve());
+    assert_eq!(12, problem.solve().unwrap_or(0));
 }
 
 #[test]
 fn test_case_1() {
     let premise = Premise { sides: vec![4, 5, 10, 20] };
     let problem = Problem { premise: premise };
-    assert_eq!(0, problem.solve()); // = never
+    assert_eq!(0, problem.solve().unwrap_or(0)); // = never
 }
