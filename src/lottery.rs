436281b5355d144339f7084e96e10e3969bcf186
Yuya <yuya.maemichi@gmail.com>
2017-06-21 01:33:43 +0900

    Solve it by binary search
    `O(n^4) -> O(n^3 log n)`

--- src/lottery.rs
+++ src/lottery.rs
@@ -9,17 +9,20 @@ struct Premise {
 
 impl Problem {
     fn solve(&self) -> bool {
-        let sum = self.premise.sum;
-        let ns = &self.premise.ns;
+        let sum = self.premise.sum as i32;
+        let ns = &mut self.premise.ns.clone();
+        ns.sort(); // for binary search
+        let ns = &ns.iter()
+                    .map(|&n| n as i16)
+                    .collect::<Vec<_>>();
 
         for n0 in ns {
             for n1 in ns {
                 for n2 in ns {
-                    for n3 in ns {
-                        let ns: Vec<_> = vec![n0, n1, n2, n3];
-                        if ns.into_iter().sum::<u16>() as u32
-                            == sum { return true }
-                    }
+                    let ns3: Vec<_> = vec![n0, n1, n2];
+                    let sum3 = ns3.into_iter().sum::<i16>() as i32;
+                    let n = (sum - sum3) as i16;
+                    if let Ok(_) = ns.binary_search(&n) { return true }
                 }
             }
         }
