c3264fc72703c312f759623c5d962eee7beb8b21
Yuya M <Wintus@users.noreply.github.com>
2017-06-21 02:26:18 +0900

    Solve/lottery (#10)
    * Add stub of lottery problem
    
    * Set premise according to problem
    
    * Solve it naively
    
    * Solve it by binary search
    
    `O(n^4) -> O(n^3 log n)`
    
    * Solve it more efficiently by reducing inner loop
    
    `O(n^3 log n) -> O(n^2 log n)`
    
    * Add TODO

---
+++ src/lottery.rs
@@ -0,0 +1,58 @@
+struct Problem {
+    premise: Premise
+}
+
+struct Premise {
+    sum: u32,
+    ns: Vec<u16>
+}
+
+impl Problem {
+    fn solve(&self) -> bool {
+        let sum = self.premise.sum as i32;
+        let ns = &self.premise.ns;
+
+        // TODO: use combination of 2
+        let ns2 = &mut Vec::new();
+        for n0 in ns {
+            for n1 in ns {
+                ns2.push(n0 + n1);
+            }
+        }
+        ns2.sort(); // for binary search
+        let ns = &ns2.iter()
+                     .map(|&n| n as i16)
+                     .collect::<Vec<_>>();
+
+        // TODO: use 直積
+        for n0 in ns {
+            for n1 in ns {
+                let ns2: Vec<_> = vec![n0, n1];
+                let sum2 = ns2.into_iter().sum::<i16>() as i32;
+                let n = (sum - sum2) as i16;
+                if let Ok(_) = ns.binary_search(&n) { return true }
+            }
+        }
+        false
+    }
+}
+
+#[test]
+fn test_case_0() {
+    let premise = Premise {
+        sum: 10,
+        ns: vec![1, 3, 5]
+    };
+    let problem = Problem { premise: premise };
+    assert!(problem.solve());
+}
+
+#[test]
+fn test_case_1() {
+    let premise = Premise {
+        sum: 9,
+        ns: vec![1, 3, 5]
+    };
+    let problem = Problem { premise: premise };
+    assert!(!problem.solve());
+}
